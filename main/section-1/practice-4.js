c124c15519e8fe86535b08ba706412b2697cf372
BubbleM <1870408473@qq.com>
2017-08-20 00:20:52 +0800

    update praceice1-4

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -5,5 +5,11 @@ function includes(collection, ch) {
 }
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return collectionA.filter(item => includes(objectB.value, item.key));
+  let result = [];
+
+  collectionA.forEach(item => {
+    if (includes(objectB.value, item.key)) result.push(item.key);
+  });
+
+  return result;
 }
